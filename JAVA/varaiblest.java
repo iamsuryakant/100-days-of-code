6f15a1733c2358420ac642a91340484838488486
iamsuryakant <suryakantthakur825@gmail.com>
2020-07-12 20:42:11 +0530

    Round 1 Day 14

---
+++ JAVA/varaiblest.java
@@ -0,0 +1,18 @@
+public class varaiblest {
+    public static void main(String[] args) {
+        String driver;
+        driver=("Hamish");
+        driver=driver.toUpperCase();
+        System.out.println(driver);
+        int x = driver.length();
+        System.out.println(x);
+        double accurate = 5/2.0;
+        System.out.println(accurate);
+        double div=5/2;
+        System.out.println(div);
+        int s=5, y=6;
+        int z= s*y;
+        System.out.println(z);
+    }
+    
+}
