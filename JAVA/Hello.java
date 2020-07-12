e92834edd5b8c26e5232648ddac8ea0e5ede8c04
iamsuryakant <suryakantthakur825@gmail.com>
2020-07-12 11:54:32 +0530

    Create Hello.java

---
+++ JAVA/Hello.java
@@ -0,0 +1,10 @@
+public class Hello  {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("JAVA, Rocks!");
+        System.out.println(2+4);
+        
+    }
+ 
+}
