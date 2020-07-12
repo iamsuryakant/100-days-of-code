512e6361a40f416bd0ef0a571ba97e88edbb2c8f
iamsuryakant <suryakantthakur825@gmail.com>
2020-07-12 12:20:13 +0530

    Create variable.java

---
+++ JAVA/variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args) {
+        int passenger;
+        passenger=0;
+        passenger = passenger + 5;
+        passenger = passenger -3;
+        passenger = passenger -1+5;
+        System.out.println(passenger);
+
+
+        
+    }
+    
+}
