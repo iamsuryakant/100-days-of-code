18ead21d79f2a12cb7392833cbc18fe389c18237
iamsuryakant <suryakantthakur825@gmail.com>
2020-07-15 20:41:18 +0530

    Create parameters.java

---
+++ JAVA/parameters.java
@@ -0,0 +1,14 @@
+public class parameters {
+
+        public static void greeting(String location) {
+
+            System.out.println("Hello," + location);
+
+        }
+
+        public static void main(String[] args) {
+            greeting("Mexico City");
+    }
+}
+
+ 
