d969b3589444b036900722a0b4ddb47bd4626e87
[iamsuryakant] <suryakantthakur825@gmail.com>
2020-12-17 19:49:26 +0530

    commit

---
+++ C++/TestArea.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle
+{
+public:
+    int length;
+    int breadth;
+    int area()
+    {
+        return length * breadth;
+    }
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+
+int main()
+{
+    Rectangle r1;
+
+    r1.length = 5;
+    r1.breadth = 10;
+
+    cout << "Area of rectangle is: " << r1.area() << endl;
+    cout << "Perimeter is: " << r1.perimeter() << endl;
+    return 0;
+}
