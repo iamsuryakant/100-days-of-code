4fee2095ff25c56fff0bad95ab9964c144421ef5
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-02-12 09:37:01 +0530

    cpp practice problem

---
+++ C++/Help Classmates/main.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+class Solution{
+
+    public:
+    vector<int> help_classmate(vector<int> arr, int n)
+    {
+        // Your code goes here
+        stack<int> s;
+    	vector<int> res(n,-1);
+
+    	for (int i = 0; i < n; i++) {
+    		if (s.empty()) {
+    			s.push(i);
+    			continue;
+    		}
+    		while (s.empty() == false && arr[s.top()] > arr[i]) {
+    			res[s.top()] = arr[i];
+    			s.pop();
+    		}
+    		s.push(i);
+    	}
+    	return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
+
+  // } Driver Code Ends
