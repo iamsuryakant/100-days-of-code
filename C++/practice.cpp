98280a42fcdc02b81229bfc716ba5197b337061a
[iamsuryakant] <suryakantthakur825@gmail.com>
2021-05-20 21:13:23 +0530

    practice

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,79 +1,42 @@
-// { Driver Code Starts
-//Initial Template for C++
-
-// CPP code to find largest and
-// second largest element in the array
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include<forward_list>
+#include<list>
 using namespace std;
 
-// } Driver Code Ends
-
-class Solution
+int getSer(int n, int k)
 {
-public:
-    /* Function to find largest and second largest element
-    *sizeOfArray : number of elements in the array
-    * arr = input array
-    */
-    vector<int> largestAndSecondLargest(int n, int arr[])
-    {
-        int max = INT_MIN, max2 = INT_MIN;
-        vector<int> v;
+    list <int> l;
 
-        for (int i = 0; i < n; i++)
+    for(int i = 0; i< n; i++)
+    {
+        l.push_back(i);
+    }
+
+    auto it = l.begin();
+
+    while(l.size()>1)
+    {
+        for(int i = 1; i < k; i++)
         {
-            if (arr[i] > max)
+            it++;
+
+            if(it == l.end())
             {
-                max2 = max;
-                max = arr[i];
-                v.push_back(max);
-            }
-            else if (arr[i] > max2 && arr[i] != max)
-            {
-                max2 = arr[i];
-                v.push_back(max2);
+                it = l.begin();
             }
         }
 
-        if(max2 == INT_MIN)
+        it = l.erase(it);
+
+        if(it == l.end())
         {
-            max2 = -1;
-            v.push_back(max2);
+            it = l.begin();
         }
-        //v.push_back(max);
-        // v.push_back(max2);
-        return v;
     }
-};
+    return *l.begin();
+}
+//using namespace std;
+int main(){
 
-// { Driver Code Starts.
-
-// Driver Code
-int main()
-{
-
-    // Testcase input
-    int testcases;
-    cin >> testcases;
-
-    // Looping through all testcases
-    while (testcases--)
-    {
-        int sizeOfArray;
-        cin >> sizeOfArray;
-
-        int arr[sizeOfArray];
-
-        // Array input
-        for (int index = 0; index < sizeOfArray; index++)
-        {
-            cin >> arr[index];
-        }
-        Solution obj;
-        vector<int> ans = obj.largestAndSecondLargest(sizeOfArray, arr);
-        cout << ans[0] << ' ' << ans[1] << endl;
-    }
-
-    return 0;
-} // } Driver Code Ends
+   cout<<getSer(7, 3);
+}
